ab509d057145540ed5fee559c87b36ab60783e13
gokdeniz2107 <karadenizgokdeniz152@gmail.com>
2025-05-28 12:14:59 +0300

    Test: baseURL sabit Railway backend URL'si olarak ayarlandı

---
+++ src/utils/api.js
@@ -0,0 +1,19 @@
+import axios from 'axios';
+
+const api = axios.create({
+  baseURL: 'https://vffapp-production.up.railway.app', // TEST: Sabit backend URL
+  headers: {
+    'Content-Type': 'application/json',
+  },
+});
+
+// Request interceptor to add auth token
+api.interceptors.request.use((config) => {
+  const token = localStorage.getItem('token');
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+  return config;
+});
+
+export default api; 
