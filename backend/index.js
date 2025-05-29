4ce3db36c8e0091c657fd05012f5ff9dc6a628d1
gokdeniz2107 <karadenizgokdeniz152@gmail.com>
2025-05-29 11:57:27 +0300

    cleanup: remove OPENAI_API_KEY debug log

--- backend/index.js
+++ backend/index.js
@@ -9,7 +9,6 @@ import elevenlabsRoutes from "./routes/elevenlabs.js";
 import googlettsRoutes from './routes/googletts.js';
 
 dotenv.config();
-console.log('OPENAI_API_KEY:', process.env.OPENAI_API_KEY);
 
 const app = express();
 app.use(cors({ origin: '*' }));
